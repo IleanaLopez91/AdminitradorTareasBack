918bda46a318255da6a607008d12f33b12b8febe
Ileana <ferchu91garcia@gmail.com>
2024-07-27 20:20:19 -0300

    creando modelo de tarea y relacionandolo con los proyectos

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,28 +1,39 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, PopulatedDoc, Types } from "mongoose";
+import { ITask } from "./Task";
 
-export type ProjectType = {
+export interface IProject extends Document {
   projectName: string;
   clientName: string;
   description: string;
-};
+  tasks: PopulatedDoc<ITask & Document>[];
+}
 
-const ProjectSchema: Schema = new Schema({
-  projectName: {
-    type: String,
-    require: true,
-    trim: true,
+const ProjectSchema: Schema = new Schema(
+  {
+    projectName: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    clientName: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    tasks: [
+      {
+        type: Types.ObjectId,
+        ref: "Task",
+      },
+    ],
   },
-  clientName: {
-    type: String,
-    require: true,
-    trim: true,
-  },
-  description: {
-    type: String,
-    require: true,
-    trim: true,
-  },
-});
+  { timestamps: true }
+);
 
-const Project = mongoose.model<ProjectType>("Project", ProjectSchema);
+const Project = mongoose.model<IProject>("Project", ProjectSchema);
 export default Project;
