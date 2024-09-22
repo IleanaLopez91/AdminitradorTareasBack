63c5f91f8ea171da31de6fcb804228bd8f879dd4
Ileana <ferchu91garcia@gmail.com>
2024-09-21 22:34:45 -0300

    instalando morgan

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import morgan from "morgan";
 import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
@@ -10,6 +11,7 @@ connectDB();
 
 const server = express();
 server.use(cors(corsConfig));
+server.use(morgan("dev"));
 server.use(express.json());
 
 server.use("/api/projects", projectRoutes);
