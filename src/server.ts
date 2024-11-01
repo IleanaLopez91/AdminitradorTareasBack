f38e78887aad9abc025e4d02ae68db460778888f
Ileana <ferchu91garcia@gmail.com>
2024-11-01 20:15:07 -0300

    creando rutas y modeles para autentificar usuarios, hay problemas con los cors

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,18 @@ import cors from "cors";
 import morgan from "morgan";
 import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
+import authRoutes from "./routes/authRoutes";
 import projectRoutes from "./routes/projectRoutes";
 
 dotenv.config();
 connectDB();
 
 const server = express();
-server.use(cors(corsConfig));
+//server.use(cors(corsConfig));
 server.use(morgan("dev"));
 server.use(express.json());
 
+server.use("/api/auth", authRoutes);
 server.use("/api/projects", projectRoutes);
 
 export default server;
