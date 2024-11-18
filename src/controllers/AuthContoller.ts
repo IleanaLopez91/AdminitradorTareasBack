b7201f98a3e722a3a69616852f13e75ed2552adf
Ileana <ferchu91garcia@gmail.com>
2024-11-18 17:07:13 -0300

    hasheando contraseña y generando token

--- src/controllers/AuthContoller.ts
+++ src/controllers/AuthContoller.ts
@@ -1,11 +1,34 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
+import Token from "../models/Token";
+import { generateToken } from "../utils/token";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
+      const { password, email } = req.body;
+
+      //Prevenir usuarios duplicados
+      const userExists = await User.findOne({ email });
+      if (userExists) {
+        const error = new Error("El usuario ya esta registrado");
+        return res.status(409).json({ error: error.message });
+      }
+
+      //Crea el usuario si no se duplica el mail
       const user = new User(req.body);
-      await user.save();
+
+      //hashear password
+      user.password = await hashPassword(password);
+
+      //Generar token
+      const token = new Token();
+      token.token = generateToken();
+      token.user = user.id;
+
+      //Almacenar ambos usuario y token
+      await Promise.allSettled([user.save(), token.save()]);
       res.send("Cuenta creada, revisa tu email para confirmarla");
     } catch (error) {
       res.status(500).json({ error: "Hubo un error" });
