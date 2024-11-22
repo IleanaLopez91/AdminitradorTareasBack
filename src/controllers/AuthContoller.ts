cb0834e1b121aa587b6c2db0aeadec8ff3bf3ec2
Ileana <ferchu91garcia@gmail.com>
2024-11-21 21:48:06 -0300

    autenticando usuario

--- src/controllers/AuthContoller.ts
+++ src/controllers/AuthContoller.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
-import { hashPassword } from "../utils/auth";
+import { checkPassword, hashPassword } from "../utils/auth";
 import Token from "../models/Token";
 import { generateToken } from "../utils/token";
 import { AuthEmail } from "../emails/AuthEmail";
@@ -49,12 +49,50 @@ export class AuthController {
       const tokenExists = await Token.findOne({ token });
       if (!tokenExists) {
         const error = new Error("Token no valido");
-        return res.status(401).json({ error: error.message });
+        return res.status(404).json({ error: error.message });
       }
       const user = await User.findById(tokenExists.user);
       user.confirmed = true;
       await Promise.allSettled([user.save(), tokenExists.deleteOne()]);
       res.send("Cuenta confirmada correctamente");
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error" });
+    }
+  };
+  static login = async (req: Request, res: Response) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+        const error = new Error("Usuario no encontrado");
+        return res.status(404).json({ error: error.message });
+      }
+      if (!user.confirmed) {
+        const token = new Token();
+        token.user = user.id;
+        token.token = generateToken();
+        await token.save();
+
+        //Enviar email
+        AuthEmail.sendConfirmationEmail({
+          email: user.email,
+          name: user.name,
+          token: token.token,
+        });
+
+        const error = new Error(
+          "La cuenta no ha sido confirmada, hemos enviado un email de confirmacion"
+        );
+        return res.status(401).json({ error: error.message });
+      }
+
+      //Revisar password
+      const isPasswordCorrect = await checkPassword(password, user.password);
+      if (!isPasswordCorrect) {
+        const error = new Error("Password Incorrecto");
+        return res.status(401).json({ error: error.message });
+      }
+      res.send("Autenticado...");
     } catch (error) {
       res.status(500).json({ error: "Hubo un error" });
     }
