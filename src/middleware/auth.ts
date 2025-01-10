06261365e5bb2ed5da4306ebe1985665d9547cc6
Ileana <ferchu91garcia@gmail.com>
2025-01-09 23:53:21 -0300

    JWT

---
+++ src/middleware/auth.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+
+export const authenticate = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const bearer = req.headers.authorization;
+  if (!bearer) {
+    const error = new Error("No autorizado");
+    return res.status(401).json({ error: error.message });
+  }
+
+  const [, token] = bearer.split(" ");
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    //console.log(decoded);
+    if (typeof decoded === "object" && decoded.id) {
+      const user = await User.findById(decoded.id);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Token no valido" });
+  }
+  next();
+};
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -5,11 +5,13 @@ import { handleInputErrors } from "../middleware/validation";
 import { TaskController } from "../controllers/TaskController";
 import { validateProjectExists } from "../middleware/project";
 import { taskBelongsToProject, validateTaskExists } from "../middleware/task";
+import { authenticate } from "../middleware/auth";
 
 const router = Router();
 
 router.post(
   "/",
+  authenticate,
   body("projectName")
     .notEmpty()
     .withMessage("El nombre del Proyecto es Obligatorio"),
